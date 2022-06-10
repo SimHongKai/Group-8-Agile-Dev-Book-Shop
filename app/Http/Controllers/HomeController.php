7a829e3e8a5394eb2db558f878e8aac9cdedf947
dbsq102 <dexterboh2002@gmail.com>
2022-06-10 22:52:45 +0800

    Commit #4: Shopping Cart UI
    Added the shopping cart UI that can retrieve and display items from the database, and recalculate prices automatically.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,71 +11,83 @@ use DB;
 
 class HomeController extends Controller
 {
-    public function shoppingCartView(Request $request){
-        if(Session::has('userId')){
-            return view("shoppingCart");
-        }else{
-            return redirect()->route('LoginUser');
-        }
-    }
-
-    public function getUserAddress(Request $request){
-        if(Session::has('userId')){
-            $userID = Session::get('userId');
-            $user = User::find($userID);
-            return $user;
-        }
-        
-        return null;
-    }
-
-    /* public function processCart(Request $request)
-    {
-        if(Session::has('userId')){
-            //Initial value
-            $initialPrice = Session::get('priceItem');
-            $initialItemCount = Session::get('numItem');
-            $userID = Session::get('userId');
-            //Get value
-            $selectedPrice = $request->addButton;
-            $ISBNValue=$request->bookISBN;
-
-            //Sum value
-            $newPrice = $initialPrice + $selectedPrice;
-            $newItemCount = $initialItemCount + 1;
-
-            //Update session
-            $request->session() -> put('numItem',$newItemCount);
-            $request->session() -> put('priceItem',$newPrice);
-
-            $data = array('qty' => $newItemCount, 'price' => $newPrice);
-            $data = json_encode($data);
-            
-            //Upload to database 
-            //check if the book exists for the user already
-            if(CartItem::where('userID',$userID)->where('ISBN13',$ISBNValue)->exists()){
-                $existingValue = CartItem::select('qty')->where('userID',$userID) ->Where('ISBN13',$ISBNValue) ->get();
-                $existingValue = preg_replace('/[^0-9]/','',$existingValue);
-                $updatedNumValue =$existingValue+1;
-                CartItem::where('userID',$userID) ->Where('ISBN13',$ISBNValue) -> update(['qty' => $updatedNumValue]);
-            }
-            else{
-                $cartItem = new CartItem();
-                $cartItem->userID = $userID;
-                $cartItem->ISBN13 = $ISBNValue;
-                $cartItem->qty = 1;
-                $res = $cartItem->save();
-            }
-
-            //Update Header
-            return redirect()->route('addCart');
-        }
-        else{
-            return redirect()->route('LoginUser');
-        }
-    } */
 
     public function processCart(Request $request)
+    {
+        if(Session::has('userId')){
+            //Get Value
+            $userID = Session::get('userId');
+            $ISBN13 = $request->bookISBN;
+            $stock = Stock::find($ISBN13);
+
+            //Sum value
+            $newPrice = $this-> calculateNewPrice($stock->retailPrice);
+            $newItemCount = $this-> calculateNewQuantity();
+
+            //Update session
+            $sessionUpdated = $this -> updateSession($newPrice,$newItemCount,$request);
+            
+            //Check Session updated
+            if($sessionUpdated){
+                //check if the book exists for the user already
+                $check = $this -> checkExist($userID,$ISBN13);
+                if($check){
+                    $this -> updateDB($userID,$ISBN13);
+                }
+                //Upload to database
+                else{
+                    $this -> uploadDB($userID,$ISBN13);
+                }
+            }
+            
+            else{
+                return redirect()->route('addCart');
+            }
+
+            // put cart data in array to be returned
+            $data = $this -> isLoggedIn ($newItemCount,$newPrice,True);
+        }
+
+        else{
+            $data = $this -> isLoggedIn (0,0,False);  
+        }
+
+        $data = json_encode($data);
+        return $data;
+    }
+
+    //function to add 1 quantity in the shopping cart
+    /*public function addQuantity(Request $request)
+    {
+        if(Session::has('userId')){
+            //Get Value
+            $userID = Session::get('userId');
+            $ISBN13 = $request->bookISBN;
+            $stock = Stock::find($ISBN13);
+
+            //Sum value
+            $newPrice = $this-> calculateNewPrice($stock->retailPrice);
+            $newItemCount = $this-> calculateNewQuantity();
+            $subtotalPrice = 0;
+            $subtotalQty = 0;
+
+            //Update session
+            $sessionUpdated = $this -> updateSession($newPrice,$newItemCount,$request);
+            
+            //Upload to database 
+            $this -> updateDBadd($userID,$ISBN13);
+
+            $subtotalPrice = $this -> calculateNewSubtotalPrice($updatedNumValue, $stock->retailPrice);
+            $subtotalQty = $updatedNumValue;
+
+            // put cart data in array to be returned
+            $data = $this -> isLoggedIn2 ($newItemCount, $newPrice, $subtotalPrice, $subtotalQty, True);
+            $data = json_encode($data);
+            
+            return $data;
+        }
+    }*/
+    public function addQuantity(Request $request)
     {
         if(Session::has('userId')){
             //Initial value
@@ -91,75 +103,209 @@ class HomeController extends Controller
             //Sum value
             $newPrice = $initialPrice + $stock->retailPrice;
             $newItemCount = $initialItemCount + 1;
+            $subtotalPrice = 0;
+            $subtotalQty = 0;
 
             //Update session
             $request->session() -> put('numItem',$newItemCount);
             $request->session() -> put('priceItem',$newPrice);
             
             //Upload to database 
-            //check if the book exists for the user already
+            $existingValue = CartItem::select('qty')->where('userID',$userID) ->Where('ISBN13',$ISBN13) ->get();
+            $existingValue = preg_replace('/[^0-9]/','',$existingValue);
+            $updatedNumValue =$existingValue+1;
 
-            //method 1
-            if(CartItem::where('userID',$userID)->where('ISBN13',$ISBN13)->exists()){
-                $existingValue = CartItem::select('qty')->where('userID',$userID) ->Where('ISBN13',$ISBN13) ->get();
-                $existingValue = preg_replace('/[^0-9]/','',$existingValue);
-                $updatedNumValue =$existingValue+1;
-                CartItem::where('userID',$userID) ->Where('ISBN13',$ISBN13) -> update(['qty' => $updatedNumValue]);
-            }
-            else{
-                $cartItem = new CartItem();
-                $cartItem->userID = $userID;
-                $cartItem->ISBN13 = $ISBN13;
-                $cartItem->qty = 1;
-                $res = $cartItem->save();
-                if($res){
-                    alert('Success', 'Successfully added a new item to the shopping cart.');
-                }
-    
-                else{
-                    alert('Failed','Failed to add new item to the shopping cart.');
-                }
-            }
+            $subtotalPrice = $updatedNumValue * $stock->retailPrice;
+            $subtotalQty = $updatedNumValue;
 
-            //method 2
-            /*$checkCartItem = CartItem::where('userID', $userID)->where('ISBN13','=',$ISBN13)->first();
-            // Update Qty if exists
-            if($checkCartItem){
-                $checkCartItem->userID = $userID;
-                $checkCartItem->ISBN13 = $ISBN13;
-                $checkCartItem->qty = $checkCartItem->qty + 1;
-            }
-            $res = $checkCartItem->save();
-            // Create new record if doesn't
-            }else {
-                $newItem = new CartItem();
-                $newItem->userID = $userID;
-                $newItem->ISBN13 = $ISBN13;
-                $newItem->qty = 1;
-                $res = $checkCartItem->save();
-            } 
-
-            if($res){
-                alert('Success', 'Successfully added a new item to the shopping cart.');
-            }
-
-            else{
-                alert('Failed','Failed to add new item to the shopping cart.');
-            }*/
-
+            CartItem::where('userID',$userID) ->Where('ISBN13',$ISBN13) -> update(['qty' => $updatedNumValue]);
 
             // put cart data in array to be returned
-            $data = array('qty' => $newItemCount, 'price' => $newPrice);
+            $data = array('qty' => $newItemCount, 'price' => $newPrice, 'subtotalPrice' => $subtotalPrice, 'subtotalQty' => $subtotalQty);
             $data = json_encode($data);
             
             return $data;
         }
     }
     
-    public function obtainShoppingCart() {
-        $shoppingCart = CartItem::select('select * from shopping_cart');
-        return view('shoppingCart')->with(compact('shoppingCart'));
+    //function to reduce quantity by 1 in the shopping cart
+    /*public function minusQuantity(Request $request)
+    {
+        if(Session::has('userId')){
+            //Get Value
+            $userID = Session::get('userId');
+            $ISBN13 = $request->bookISBN;
+            $stock = Stock::find($ISBN13);
+
+            //Sum value
+            $newPrice = $this-> calculateNewPrice($stock->retailPrice);
+            $newItemCount = $this-> calculateNewQuantity();
+            $subtotalPrice = 0;
+            $subtotalQty = 0;
+
+            //Update session
+            $sessionUpdated = $this -> updateSession($newPrice,$newItemCount,$request);
+            
+            //Upload to database 
+            $this -> updateDBminus($userID,$ISBN13);
+
+            $subtotalPrice = $this -> calculateNewSubtotalPrice($updatedNumValue, $stock->retailPrice);
+            $subtotalQty = $updatedNumValue;
+
+            // put cart data in array to be returned
+            $data = $this -> isLoggedIn2 ($newItemCount, $newPrice, $subtotalPrice, $subtotalQty, True);
+            $data = json_encode($data);
+            
+            return $data;
+        }
+    }*/
+    public function minusQuantity(Request $request)
+    {
+        if(Session::has('userId')){
+            //Initial value
+            $initialPrice = Session::get('priceItem');
+            $initialItemCount = Session::get('numItem');
+            $userID = Session::get('userId');
+            // Get ISBN13
+            $ISBN13 = $request->bookISBN;
+
+            // Get stock data
+            $stock = Stock::find($ISBN13);
+
+            //Sum value
+            $newPrice = $initialPrice - $stock->retailPrice;
+            $newItemCount = $initialItemCount - 1;
+            $subtotalPrice = 0;
+            $subtotalQty = 0;
+
+            //Update session
+            $request->session() -> put('numItem',$newItemCount);
+            $request->session() -> put('priceItem',$newPrice);
+            
+            //Upload to database 
+            $existingValue = CartItem::select('qty')->where('userID',$userID) ->Where('ISBN13',$ISBN13) ->get();
+            $existingValue = preg_replace('/[^0-9]/','',$existingValue);
+            $updatedNumValue =$existingValue-1;
+            CartItem::where('userID',$userID) ->Where('ISBN13',$ISBN13) -> update(['qty' => $updatedNumValue]);
+
+            $subtotalPrice = $updatedNumValue * $stock->retailPrice;
+            $subtotalQty = $updatedNumValue;
+
+            // put cart data in array to be returned
+            $data = array('qty' => $newItemCount, 'price' => $newPrice, 'subtotalPrice' => $subtotalPrice, 'subtotalQty' => $subtotalQty);
+            $data = json_encode($data);
+            
+            return $data;
+        }
+    }
+
+    //------------------------------------------------------------LOGGED IN------------------------------------------------------------
+    public function isLoggedIn($newItemCount,$newPrice,$loggedIn){
+        $data = array('qty' => $newItemCount, 'price' => $newPrice, 'login' => $loggedIn);
+        return $data;
+    }
+
+    /*public function isLoggedIn2($newItemCount, $newPrice, $subtotalPrice, $subtotalQty, $loggedIn){
+        $data = array('qty' => $newItemCount, 'price' => $newPrice, 'subtotalPrice' => $subtotalPrice, 'subtotalQty' => $subtotalQty, 'login' => $loggedIn);
+        return $data;
+    }*/
+
+    //------------------------------------------------------------CALCULATION------------------------------------------------------------
+    public function calculateNewPrice($itemPrice){
+        $initialPrice = Session::get('priceItem');
+        $newPrice = $initialPrice+$itemPrice;
+        return $newPrice;
+    }
+
+    public function calculateNewQuantity(){
+        $initialItemCount = Session::get('numItem');
+        $newQuantity=$initialItemCount+1;
+        return $newQuantity;
+    }
+
+    /*public function calculateNewQuantityMinus() {
+        $initialItemCount = Session::get('numItem');
+        $newQuantity=$initialItemCount-1;
+        return $newQuantity;
+    }
+
+    public function calculateNewSubtotalPrice($updatedNumValue, $itemPrice) {
+        $subtotalPrice = $updatedNumValue * $itemPrice;
+        return $subtotalPrice;
+    }*/
+
+    //----------------------------------------------------------UPDATE SESSION------------------------------------------------------------
+    public function updateSession($newPrice,$newQty){
+        Session:: put('numItem',$newQty);
+        Session:: put('priceItem',$newPrice);
+        return true;
+    }
+
+    //-----------------------------------------------------CHECK BOOK EXIST FOR USER-------------------------------------------------------
+    public function checkExist($userID,$ISBN13){
+        if(CartItem::where('userID',$userID)->where('ISBN13',$ISBN13)->exists()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    //-----------------------------------------------------UPDATE DATABASE-------------------------------------------------------
+    public function updateDB($userID,$ISBN13){
+        $existingValue = CartItem::select('qty')->where('userID',$userID) ->Where('ISBN13',$ISBN13) ->get();
+        $existingValue = preg_replace('/[^0-9]/','',$existingValue);
+        $updatedNumValue =$existingValue+1;
+        CartItem::where('userID',$userID) ->Where('ISBN13',$ISBN13) -> update(['qty' => $updatedNumValue]);
+    }
+
+    /*public function updateDBadd($userID,$ISBN13){
+        $existingValue = CartItem::select('qty')->where('userID',$userID) ->Where('ISBN13',$ISBN13) ->get();
+        $existingValue = preg_replace('/[^0-9]/','',$existingValue);
+        $updatedNumValue =$existingValue+1;
+        CartItem::where('userID',$userID) ->Where('ISBN13',$ISBN13) -> update(['qty' => $updatedNumValue]);
+        return $updatedNumValue;
+    }
+
+    public function updateDBminus($userID,$ISBN13){
+        $existingValue = CartItem::select('qty')->where('userID',$userID) ->where('ISBN13',$ISBN13) ->get();
+        $existingValue = preg_replace('/[^0-9]/','',$existingValue);
+        $updatedNumValue =$existingValue-1;
+        CartItem::where('userID',$userID) ->where('ISBN13',$ISBN13) -> update(['qty' => $updatedNumValue]);
+        return $updatedNumValue;
+    }*/
+
+    //-----------------------------------------------------UPLOAD DATABASE-------------------------------------------------------
+    public function uploadDB($userID,$ISBN13){
+        $cartItem = new CartItem();
+        $cartItem->userID = $userID;
+        $cartItem->ISBN13 = $ISBN13;
+        $cartItem->qty = 1;
+        $res = $cartItem->save();
+    }
+
+    //-----------------------------------------------------OTHER PAGES-------------------------------------------------------
+    public function shoppingCartView(Request $request){
+        if(Session::has('userId')){
+            $userID = Session::get('userId');
+            $shoppingCart = DB::table('shopping_cart')
+            ->select('shopping_cart.qty', 'shopping_cart.ISBN13', 'shopping_cart.userID', 'stock.coverImg', 'stock.retailPrice')
+            ->join('stock', 'shopping_cart.ISBN13', '=', 'stock.ISBN13')
+            ->where('shopping_cart.userID', '=', $userID)
+            ->get();
+            return view("shoppingCart")->with(compact('shoppingCart'));
+        }else{
+            return redirect()->route('LoginUser');
+        }
+    }
+
+    public function getUserAddress(Request $request){
+        if(Session::has('userId')){
+            $userID = Session::get('userId');
+            $user = User::find($userID);
+            return $user;
+        }
+        
+        return null;
     }
 }
-
-//add to database and get value of user id
--- resources/views/shoppingCart.blade.php
+++ resources/views/shoppingCart.blade.php
@@ -9,6 +9,7 @@
     <title>Shopping Cart</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 
 <body>
@@ -16,30 +17,58 @@
     <div class="add-stock-container">
         <div id='add-stock-content'>
             <div id = 'add-stock-form'>
-            <div id='add-stock-form'>
                 <h1><font face='Impact'>Shopping Cart</font></h1>
-                <table>
+                <table class = "shopping-cart-table">
                     <tr>
                         <th>Book</th>
-                        <th>Price Per Unit</th>
+                        <th>Price By Unit</th>
                         <th>Quantity</th>
                         <th>Total Price</th>
-                        <th>Delete</th>
+                        <th>Remove</th>
                     </tr>
-                    @csrf
-                    @foreach ($shoppingCart as $shoppingCart)
-                    <tr>    
-                        <th><img src="no_book_cover.jpg"></th>
-                        <th>10</th>
-                        <th>{{ $shoppingCart-> qty}}</th>
-                        <th>10</th>
-                        <th><a href="<?php echo url('shoppingCart') ?>"  style="text-align:right; padding-top: 8px;">
-                            <img src="{{ asset('images/delete_button.png') }}" width="40px" height="40px"></a></th>
+                    @if(!$shoppingCart->isEmpty())
+                    @foreach($shoppingCart as $shoppingCarts) 
+                    <?php
+                    $price = Session::get('priceItem');
+                    $itemCount = Session::get('numItem');
+                    ?>
                     <tr>
+                    <td><img src="{{ asset('book_covers')}}/{{$shoppingCarts->coverImg }}" width="150px" height="200px"></td>
+                        <td>{{ $shoppingCarts -> retailPrice }}</td>
+                        <td>
+                            <!--Add quantity-->
+                            <a onclick="addQuantity({{ $shoppingCarts->ISBN13 }})"><img src="{{ asset('images/add_image.png') }}" width="20px" height="20px"></a>
+                            <!--Current quantity-->
+                            <p id ="{{ $shoppingCarts->ISBN13}}Qty">{{ $shoppingCarts -> qty }}</p>
+                            <!--Minus quantity-->
+                            <a onclick="minusQuantity({{ $shoppingCarts->ISBN13 }})"><img src="{{ asset('images/minus_image.png') }}"width="20px" height="20px"> </a>
+                        </td>
+                        <td id = "{{ $shoppingCarts->ISBN13}}Price"><p>RM{{ $shoppingCarts -> retailPrice * $shoppingCarts -> qty }}</p></td>
+                        <td>
+                            <a href="<?php echo url('shoppingCart') ?>"><img src="{{ asset('images/remove_button.jpg') }}"width="40px" height="40px"> </a> 
+                        </td>
+                    </tr>
                     @endforeach
-                </table>
-                </div>
+                    <!-- retrieve item quantity and total price-->
+                    <?php
+                    $price = Session::get('priceItem');
+                    $itemCount = Session::get('numItem');
+                    ?>
+                    <tr>
+                        <th></th>
+                        <th>Total:</th>
+                        <th><?php echo $itemCount ?> items</th>
+                        <th>RM<?php echo $price ?></th>
+                        <th></th>
+                    </tr>
 
+                </table>
+                @else
+                    <p>No items in the shopping cart</p>
+                @endif
+                        
+            </div>
+            <div id = 'add-stock-form'>
                 <h1><font face='Impact'>Shipping Address</font></h1>
                 <form action="{{route('add-stock')}}" method="post" enctype="multipart/form-data">
                     <!-- Print error message that stock was NOT updated -->
@@ -64,7 +93,6 @@
                     <br>
                 </form> 
             </div>
-        </div>
             @include('footer')
         </div>
     </div>
@@ -73,6 +101,77 @@
     </script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript">
     </script>
+
+    <script>
+        function addQuantity(ISBN13){
+        fetch('shoppingCart/add-quantity', {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json, text-plain, */*",
+                "X-Requested-With": "XMLHttpRequest",
+                "X-CSRF-Token": $('meta[name="csrf_token"]').attr('content')
+            },
+            body: JSON.stringify({bookISBN: ISBN13}),
+            method: 'post',
+            credentials: "same-origin",})
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            if (response){
+                var cartQty = document.getElementById('cartQty');
+                var cartPrice = document.getElementById('cartPrice');
+                var itemQty = document.getElementById(ISBN13 + 'Qty');
+                var itemPrice = document.getElementById(ISBN13 + 'Price');
+
+                console.log(response);
+                cartQty.innerHTML = response.qty;
+                cartPrice.innerHTML = "RM" + response.price;
+                itemQty.innerHTML = response.subtotalQty;
+                itemPrice.innerHTML = "RM" + response.subtotalPrice;
+            }
+        })
+        .catch(function(error){
+            console.log(error)
+        });    
+    }
+    </script>
+
+    <script>
+        function minusQuantity(ISBN13){
+        fetch('shoppingCart/minus-quantity', {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json, text-plain, */*",
+                "X-Requested-With": "XMLHttpRequest",
+                "X-CSRF-Token": $('meta[name="csrf_token"]').attr('content')
+            },
+            body: JSON.stringify({bookISBN: ISBN13}),
+            method: 'post',
+            credentials: "same-origin",})
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            if (response){
+                var cartQty = document.getElementById('cartQty');
+                var cartPrice = document.getElementById('cartPrice');
+                var itemQty = document.getElementById(ISBN13 + 'Qty');
+                var itemPrice = document.getElementById(ISBN13 + 'Price');
+
+                console.log(response);
+                cartQty.innerHTML = response.qty;
+                cartPrice.innerHTML = "RM" + response.price;
+                itemQty.innerHTML = response.subtotalQty;
+                itemPrice.innerHTML = "RM" + response.subtotalPrice;
+            }
+        })
+        .catch(function(error){
+            console.log(error)
+        });    
+    }
+    </script>
+
     <script type="text/javascript">
         // detect Country API
         document.body.onload = function() {
