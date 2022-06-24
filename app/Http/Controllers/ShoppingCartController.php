b8638e38c8ebbbac11a2a299ff66a52a9a1eab9f
SimHongKai <63938461+SimHongKai@users.noreply.github.com>
2022-06-25 07:54:50 +0800

    Fix Item Removed From Shipping Cart Notification
    - Fixed a Bug that caused array to be overridden instead of append
    - Added stylesheet to Checkout

--- app/Http/Controllers/ShoppingCartController.php
+++ app/Http/Controllers/ShoppingCartController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\CartItem;
 use App\Models\User;
+use App\Models\Postage;
 use App\Models\Stock;
 use Session;
 use DB;
@@ -57,6 +58,74 @@ class ShoppingCartController extends Controller
             'Postal' => 'required|min:3|max:50|',
             'Address' => 'required|min:0|max:200|',
         ]);
+    }
+
+    public function checkoutView(Request $request){
+        $insufficientStock = $this->adjustOutofStock();
+        if(Session::has('userId')){
+            $userID = Session::get('userId');
+            $shoppingCart = DB::table('shopping_cart')
+            ->select('shopping_cart.qty', 'stock.bookName', 'shopping_cart.ISBN13', 'shopping_cart.userID', 'stock.coverImg', 'stock.retailPrice')
+            ->join('stock', 'shopping_cart.ISBN13', '=', 'stock.ISBN13')
+            ->where('shopping_cart.userID', '=', $userID)
+            ->get();
+            $postage = Postage::get();
+            return view("checkout")->with(compact('shoppingCart'))->with(compact('postage'))->with(compact('insufficientStock'));
+        }else{
+            return redirect()->route('checkout');
+        }
+    }
+
+    public function adjustOutofStock(){
+        $userId = $this->getSessionUserId();
+        $newQty=0;
+        $newPrice=0;
+        $res=FALSE;
+        $insufficientStock = array();
+
+        $shoppingCartItems = DB::table('shopping_cart')
+            ->select('shopping_cart.qty', 'shopping_cart.ISBN13', 'shopping_cart.userID')
+            ->where('shopping_cart.userID', '=', $userId)
+            ->get();
+    
+        foreach($shoppingCartItems as $shoppingCarts) {
+            $existingCartValue = CartItem::select('qty')->where('userID',$userId) ->Where('ISBN13',$shoppingCarts->ISBN13) ->get();
+            $existingCartValue = preg_replace('/[^0-9.]/','',$existingCartValue);
+
+            $StockCartValue = Stock::select('qty')->Where('ISBN13',$shoppingCarts->ISBN13) ->get();
+            $StockCartValue = preg_replace('/[^0-9.]/','',$StockCartValue);
+
+            if($existingCartValue>$StockCartValue){
+                $newQty = $newQty + $StockCartValue;
+                CartItem::where('userID',$userId) ->Where('ISBN13',$shoppingCarts->ISBN13) -> update(['qty' => $StockCartValue]);
+                $itemRetailPrice = Stock::select('retailPrice')->Where('ISBN13',$shoppingCarts->ISBN13) ->get();
+                $itemRetailPrice = preg_replace('/[^0-9.]/','',$itemRetailPrice);
+                $newPrice = ($itemRetailPrice * $StockCartValue)+$newPrice;
+                $qtyChanged = $existingCartValue - $StockCartValue;
+                
+                $bookName = Stock::select('bookName')->Where('ISBN13',$shoppingCarts->ISBN13) ->get();
+                $bookName = preg_replace('/\bbookName\b/', '',$bookName);
+                $bookName = preg_replace('/[^A-Za-z0-9 \-]/', '',$bookName);
+                $insufficientStock[$bookName] = $qtyChanged;
+            }
+
+            else{
+                $newQty = $newQty + $StockCartValue;
+                $itemRetailPrice = Stock::select('retailPrice')->Where('ISBN13',$shoppingCarts->ISBN13) ->get();
+                $itemRetailPrice = preg_replace('/[^0-9.]/','',$itemRetailPrice);
+                $newPrice = ($itemRetailPrice * $StockCartValue)+$newPrice;
+            }
+        }
+        
+        Session:: put('numItem',$newQty);
+        Session:: put('priceItem',$newPrice);
+        $data = $this -> updateHeader ($newQty,$newPrice,True);
+        return $insufficientStock;
+    }
+
+    public function updateHeader($newItemCount,$newPrice,$loggedIn){
+        $data = array('qty' => $newItemCount, 'price' => $newPrice, 'login' => $loggedIn);
+        return $data;
     }
 }
 ?>
